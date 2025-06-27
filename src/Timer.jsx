c46ef57832ec718be693c2f288aa0606ed11efb3
Delia Holman <delia.b.holman@gmail.com>
2025-06-27 15:33:08 -0500

    Timer countdown logic added

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import './Timer.css'
 import backIcon from './assets/back.svg';
@@ -5,13 +6,40 @@ import backIcon from './assets/back.svg';
 function Timer() {
     const { study, breakTime } = useParams();
     const navigate = useNavigate();
+
+    // string to numbers
     const studyTime = parseInt(study);
     const breakDuration = parseInt(breakTime);
+
+    // store time left
+    const [timeLeft, setTimeLeft] = useState(studyTime * 60);
+
+    // convert timeLeft to MM:SS format
+    const formatTime = (totalSeconds) => {
+        const minutes = Math.floor(totalSeconds / 60);
+        const seconds = totalSeconds % 60;
+        // if seconds is single digit then minute is 0
+        return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    };
+
+    useEffect(() => {
+        // only run the timer if time is remaining
+        if (timeLeft <= 0) return;
+    
+        // setting up a 1 second interval
+        const timerId = setInterval(() => {
+          // decrease the time left by 1 second
+          setTimeLeft((prevTime) => prevTime - 1);
+        }, 1000);
+    
+        // clear the 1 second interval when time updates
+        return () => clearInterval(timerId);
+    }, [timeLeft]); // run again if timeLeft changes
 
     return (
         <div id="timer-widget">
 
-            <h1 className="time-display"><span id="timer">{study}:00</span></h1>
+            <h1 className="time-display"><span id="timer">{formatTime(timeLeft)}</span></h1>
 
             <div id="time-labels">
                 <button id="working">WORKING</button>
